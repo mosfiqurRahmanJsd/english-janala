c63502ccfa0a03c831a63c4c8e1360df0a340532
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2025-11-25 08:49:09 +0600

    show dfault section

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 
 
-const loadLesson = () => {
+const loadLessonBtn = () => {
     fetch('https://openapi.programming-hero.com/api/levels/all')
         .then(res => res.json())
         .then(data => showLessonBtn(data.data));
@@ -14,24 +14,40 @@ const showLessonBtn = (lessons) => {
         
         const lessonDiv = document.createElement('span');
         lessonDiv.innerHTML = `
-            <button onclick="showLesson('${lesson.level_no}')" class="btn btn-outline mx-2 btn-primary font-poppins font-semibold"><i
+            <button onclick="showLessonSection('${lesson.level_no}')"  class="btn btn-outline mx-2 btn-primary font-poppins font-semibold"><i
                             class="fa-brands fa-leanpub"></i> Lesson - ${lesson.level_no}</button>
         `
         lessonBtnContainer.appendChild(lessonDiv);
     });
 }
 
+const showLessonSection = (id) => {
+    if(id) {
+        loadLesson(id); 
+        document.getElementById('default-lesson').classList.add('hidden'); 
+    }
 
-const showLesson = (id) => {
+    defaultSection.classList.remove('hidden'); 
+
+    // onclick="loadLesson('${lesson.level_no}')"
+}
+
+
+const loadLesson = (id) => {
     fetch(`https://openapi.programming-hero.com/api/level/${id}`)
     .then(res => res.json())
-    .then(data => console.log(data));   
+    .then(data => showLesson(data.data));   
+}
+
+
+const showLesson = (lessons) =>  {
+    lessons.forEach(lesson => {
+        console.log(lesson); 
+    })
 }
 
 
 
 
 
-
-
-loadLesson();
+loadLessonBtn();
