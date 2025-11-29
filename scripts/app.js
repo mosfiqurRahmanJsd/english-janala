780ba185764d9255c6f6d041317db8f4bf9c4628
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2025-11-29 17:25:35 +0600

    some bug fixed

--- scripts/app.js
+++ scripts/app.js
@@ -139,7 +139,6 @@ function pronounceWord(word) {
     const utterance = new SpeechSynthesisUtterance(word);
     utterance.lang = 'en-EN';
     window.speechSynthesis.speak(utterance);
-    console.log('lesson')
 }
 
 
