21dc3cc5f5cad86ed65137682edb1dded3ba1a81
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2025-11-29 08:36:33 +0600

    assignment finished

--- scripts/app.js
+++ scripts/app.js
@@ -41,14 +41,17 @@ const loadDetails = (id) => {
 document.getElementById('loginBtn').addEventListener('click', function () {
     const userName = document.getElementById('user-name').value;
     const password = document.getElementById('password').value;
-    if (userName.length >= 2 && password == "123456") {
+
+    if (!userName.length) {
+        alert('Please tell your name first!')
+    } else if (password !== "123456") {
+        alert('Wrong password. Please contact admin to get Your Login Code')
+    } else {
         document.getElementById('header').classList.remove('hidden');
         document.getElementById('learn').classList.remove('hidden');
         document.getElementById('faq').classList.remove('hidden');
         document.getElementById('banner').classList.add('hidden');
 
-        document.getElementById('user-name').value = "";
-        document.getElementById('password').value = "";
     }
 })
 
@@ -62,9 +65,9 @@ const removeActiveClass = () => {
 
 // smooth scrolling 
 document.querySelectorAll('a.scroll-link').forEach(link => {
-    link.addEventListener('click', function(e) {
-        e.preventDefault(); 
-        const target = document.querySelector(this.getAttribute('href')); 
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
         target.scrollIntoView({
             behavior: "smooth"
         })
@@ -90,12 +93,14 @@ const showLessonBtn = (lessons) => {
                             class="fa-brands fa-leanpub"></i> Lesson - ${lesson.level_no}</button>
         `
         lessonBtnContainer.appendChild(lessonDiv);
+
     });
 }
 
 const showLessonSection = (id) => {
     removeActiveClass();
 
+    document.getElementById('loading-bar').classList.remove('hidden');
     const activeLessonBtn = document.getElementById(id);
     activeLessonBtn.classList.add('active');
 
@@ -117,14 +122,18 @@ const showLessonSection = (id) => {
 
 const showEmptyLesson = (data) => {
     data.length === 0 ? document.getElementById('lesson-not-found').classList.remove('hidden') : document.getElementById('lesson-not-found').classList.add('hidden')
+    document.getElementById('loading-bar').classList.add('hidden');
 }
 
 
 const showLesson = (lessons) => {
 
     const cardContainer = document.getElementById('card-container')
+    cardContainer.classList.remove('hidden');
+    console.log(lessons);
 
     cardContainer.innerHTML = "";
+
     lessons.forEach(lesson => {
         // console.log(lesson); 
         const div = document.createElement('div');
@@ -135,16 +144,17 @@ const showLesson = (lessons) => {
             <div class="text-center mb-14">
                 <h3 class="text-4xl font-bold font-inter mb-6">${lesson.word}</h3>
                 <h5 class="text-xl font-medium font-inter mb-6">Meaning /Pronunciation</h5>
-                <h2 class="text-4xl font-semibold font-hind-siliguri text-[#464649]"> “${lesson.meaning? lesson.meaning: `অর্থ নেই`} / ${lesson.pronunciation? lesson.pronunciation: `অর্থ নেই`}” </h2>
+                <h2 class="text-4xl font-semibold font-hind-siliguri text-[#464649]"> “${lesson.meaning ? lesson.meaning : `অর্থ নেই`} / ${lesson.pronunciation ? lesson.pronunciation : `অর্থ নেই`}” </h2>
             </div>
             <div class="flex justify-between">
                 <button id="${lesson.id}" onclick="loadDetails('${lesson.id}')" class="bg-[#E8F4FF] p-4 rounded"><i class="fa-solid fa-circle-info text-2xl "></i></button>
-                <button class="bg-[#E8F4FF] p-4 rounded"><i class="fa-solid fa-volume-high text-2xl"></i></button>
+                <button onclick="pronounceWord('${lesson.word}')" class="bg-[#E8F4FF] p-4 rounded"><i class="fa-solid fa-volume-high text-2xl"></i></button>
             </div>
 
         `
         cardContainer.appendChild(div);
 
+        document.getElementById('loading-bar').classList.add('hidden');
     })
 }
 
@@ -160,17 +170,17 @@ const showDetails = (details) => {
         <div class="border-2 border-[#EDF7FF] p-6 rounded-xl">
                             <h3 class="font-poppins text-4xl font-semibold mb-8">${details.word} (<i class="fa-solid fa-microphone-lines"></i> : ${details.meaning})</h3>
                             <h4 class="font-medium text-2xl font-poppins mb-2.5">Meaning</h4>
-                            <h4 class="font-hind-siliguri font-medium text-2xl mb-8">${details.meaning}</h4>
+                            <h4 class="font-hind-siliguri font-medium text-2xl mb-8">${details.meaning === null ? `অর্থ পাওয়া যায়নি` : details.meaning}</h4>
                             <h4 class="font-poppind text-2xl font-semibold mb-2.5">Example</h4>
                             <p class="text-[#333333] text-2xl font-normal mb-8">${details.sentence}</p>
                             <h4 class="font-hind-siliguri text-2xl font-medium mb-2.5">সমর্থক শব্দগুলো</h4>
 
                             <div class="flex gap-4" id="synonyms">
                                ${details.synonyms.map(syn =>
+        `<p class="px-5 py-2 bg-[#EDF7FF] border-2 border-[#D7E4EF] rounded">${syn === undefined ? `""` : syn}</p>`
 
-        `<p class="px-5 py-2 bg-[#EDF7FF] border-2 border-[#D7E4EF] rounded">${syn}</p>`
-    )
-        }
+    ).join('')}
+        
 
                             </div>
 
@@ -187,6 +197,13 @@ const showDetails = (details) => {
 }
 
 
+// listen this word
+
+function pronounceWord(word) {
+    const utterance = new SpeechSynthesisUtterance(word); 
+    utterance.lang = 'en-EN'; 
+    window.speechSynthesis.speak(utterance); 
+}
 
 
 
